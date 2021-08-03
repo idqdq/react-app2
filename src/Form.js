718287ea3501043c595b57a15624b7d853af5f5b
IDQDD <nryzhkov@gmail.com>
2021-08-03 18:11:59 +0300

    form validators. begin

--- src/Form.js
+++ src/Form.js
@@ -9,10 +9,14 @@ class Form extends Component {
         svi_descr: '',
         vrf: '',
         mgroup: '',
+        errors: { 
+            vlan_id: '',
+            vni: ''},
     }    
 
     state = this.props.evpn ? this.props.evpn : this.initialState;
     index = this.props.index;
+    formValid = true;
 
     handleChange = (event) => {
         const { name, value } = event.target
@@ -22,67 +26,112 @@ class Form extends Component {
         })
     }
 
+    handleBlur = (event) => {
+        const { name, value } = event.target
+
+        this.setState({[name]: value,}, 
+            () => this.validateField(name, value))
+    }
+
     submitForm = () => {        
         this.props.handleSubmit(this.state, this.index);
         this.setState(this.initialState)        
     }
 
+    validateField(name, value){        
+        const errors = {};                
+
+        switch(name) {
+            case 'vlan_id':                
+                if(!(!isNaN(value) && value >1 && value < 4096)) {
+                    errors[name] = 'should be a number from 1 to 4096';                    
+                }
+                break;
+
+            case 'vni':
+                if(!(!isNaN(value) && value > 10000 && value < 10999)) {
+                    errors[name] = 'should be a number from 10000 to 10999';                    
+                }
+                break;
+            default:
+                break;
+        }
+        
+        this.setState({errors: errors});        
+    }
+
     render() {
         const { vlan_id, vni, vlan_name, svi_ip, svi_descr, vrf, mgroup } = this.state;
 
-        return (
-            <form>
+        return (            
+            <form>                
                 <label htmlFor="vlan_id">vlan_id</label>
                 <input
-                    type="text"
+                    type="number"
                     name="vlan_id"
                     id="vlan_id"
                     value={vlan_id}
-                    onChange={this.handleChange} />
+                    placeholder="10"
+                    onChange={this.handleChange}
+                    onBlur = {this.handleBlur} />
+                    <span style={{color: "red"}}>{this.state.errors["vlan_id"]}</span>
                 <label htmlFor="vni">vni</label>
                 <input
-                    type="text"
+                    type="number"
                     name="vni"
                     id="vni"
                     value={vni}
-                    onChange={this.handleChange} />
+                    placeholder="10010"
+                    onChange={this.handleChange}
+                    onBlur = {this.handleBlur} />
+                    <span style={{color: "red"}}>{this.state.errors["vni"]}</span>
                 <label htmlFor="vlan_name">vlan_name</label>
                 <input
                     type="text"
                     name="vlan_name"
                     id="vlan_name"
                     value={vlan_name}
-                    onChange={this.handleChange} />
+                    placeholder="vlan10"
+                    onChange={this.handleChange}
+                    onBlur = {this.handleBlur} />
                 <label htmlFor="svi_ip">svi_ip</label>
                 <input
                     type="text"
                     name="svi_ip"
                     id="svi_ip"
                     value={svi_ip}
-                    onChange={this.handleChange} />
+                    placeholder="10.1.10.254/24"
+                    onChange={this.handleChange}
+                    onBlur = {this.handleBlur} />
                 <label htmlFor="svi_descr">svi_descr</label>
                 <input
                     type="text"
                     name="svi_descr"
                     id="svi_descr"
                     value={svi_descr}
-                    onChange={this.handleChange} />
+                    placeholder="test10 subnet"
+                    onChange={this.handleChange}
+                    onBlur = {this.handleBlur} />
                 <label htmlFor="vrf">vrf</label>
                 <input
                     type="text"
                     name="vrf"
                     id="vrf"
                     value={vrf}
-                    onChange={this.handleChange} />
+                    placeholder="Tenant-1"
+                    onChange={this.handleChange}
+                    onBlur = {this.handleBlur} />
                 <label htmlFor="mgroup">mgroup</label>
                 <input
                     type="text"
                     name="mgroup"
                     id="mgroup"
                     value={mgroup}
-                    onChange={this.handleChange} />
-                <input type="button" value="Submit" onClick={this.submitForm} />                
-            </form>
+                    placeholder="231.0.0.10"
+                    onChange={this.handleChange}
+                    onBlur = {this.handleBlur} />
+                <input type="button" value="Submit" onClick={this.submitForm} disabled={!this.formValid} />                
+            </form>            
         );
     }
 }
